e9e84fe537ddd02eab8471b74a7b68f63b4ece2f
martin.castello <castellomn94@gmail.com>
2018-06-03 15:16:06 -0300

    Se actualiza run.sh

--- src/test/run.sh
+++ src/test/run.sh
@@ -4,14 +4,6 @@ cd $(dirname $0)
 
 #cd ../complete
 
-mvn install-file
--Dfile="gs-accessing-data-mysql-0.1.0.jar"
--DgroupId="org.springframework"
--DartifactId="gs-mysql-data"
--Dversion="0.1.0"
--Dpackaging="jar"
--DlocalRepositoryPath="lib"
-
 mvn clean package
 ret=$?
 if [ $ret -ne 0 ]; then
