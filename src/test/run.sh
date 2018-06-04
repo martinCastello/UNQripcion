56c852e9d05f45295fd34f93d6769a7a75255511
martin.castello <castellomn94@gmail.com>
2018-06-03 22:29:04 -0300

    Se actualiza run.sh

--- src/test/run.sh
+++ src/test/run.sh
@@ -4,19 +4,19 @@ cd $(dirname $0)
 
 #cd ../complete
 
-#mvn clean package
-#ret=$?
-#if [ $ret -ne 0 ]; then
-#  exit $ret
-#fi
-#rm -rf target
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+rm -rf target
 
-#./gradlew build
-#ret=$?
-#if [ $ret -ne 0 ]; then
-#  exit $ret
-#fi
-#rm -rf build
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+rm -rf build
 
 #cd ../initial
 
